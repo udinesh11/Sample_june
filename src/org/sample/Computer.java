7d27019510e18f447955eac6979ae55f0c5915bd
udinesh1706 <udinesh1706@gmail.com>
2022-06-15 22:10:31 +0530

    line added

--- src/org/sample/Computer.java
+++ src/org/sample/Computer.java
@@ -3,7 +3,8 @@ package org.sample;
 public class Computer {
 	public void computermodel() {
 		System.out.println("computer model is Lenovo");
-
+    
+		System.out.println("computer model is hp");
 	}
 
 }
